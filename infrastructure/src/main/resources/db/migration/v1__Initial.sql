7fd1ac23a241790bd8f59854d0b7ed7c586f8f9c
GustavoMarques94 <marques.lameirao@gmail.com>
2024-01-16 23:40:11 -0300

    Criação das migrations da entidade categoria

--- infrastructure/src/main/resources/db/migration/v1__Initial.sql
+++ infrastructure/src/main/resources/db/migration/v1__Initial.sql
@@ -1,4 +1,10 @@
 
 CREATE TABLE category(
-    id BINARY(16) PRIMARY KEY
+    id VARCHAR(36) NOT NULL PRIMARY KEY, --PK sinaliza que precisa criar uma UNIQUE, uma UNIQUE já é um index, então não é necessário criar um index
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(4000),
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    deleted_at DATETIME(6) NULL
 );
