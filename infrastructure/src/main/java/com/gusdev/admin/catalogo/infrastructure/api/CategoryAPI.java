ffc57b785265240b9839fd0b82cc24f042a7174d
GustavoMarques94 <marques.lameirao@gmail.com>
2024-02-04 16:46:31 -0300

    Incluindo a biblioteca springdocs openapi

--- infrastructure/src/main/java/com/gusdev/admin/catalogo/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/gusdev/admin/catalogo/infrastructure/api/CategoryAPI.java
@@ -3,12 +3,17 @@ package com.gusdev.admin.catalogo.infrastructure.api;
 //Vamos difinir quais o métodos a API irá expor, e também a parte de documentação com springDocs
 
 import com.gusdev.admin.catalogo.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "categories") //tanto faz colocar com / ou sem /, o spring entende e vai tratar isso
+@Tag(name = "Categories") //Nome do Resource que estamos espondo
 public interface CategoryAPI {
 
     //ResponseEntity genérico <?>
@@ -17,9 +22,21 @@ public interface CategoryAPI {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     //@ResponseStatus(HttpStatus.CREATED) //É uma boa prática inserir o código de retorno, porém como estamos retornando um ResponseEntity não precisa declarar, pois o statusCod Http está no ResponseEntity
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value = {
+           @ApiResponse(responseCode = "201", description = "Created successfully"),
+           @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+           @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    })
     ResponseEntity<?> createCategory();
 
     @GetMapping
+    @Operation(summary = "List all categories paginated")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Listed successfully"),
+            @ApiResponse(responseCode = "422", description = "A invalid parameter was received"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    })
     Pagination<?> listCategories(
             @RequestParam(name = "search", required = false, defaultValue = "") final String search,
             @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
--- infrastructure/src/main/java/com/gusdev/admin/catalogo/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/gusdev/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -1,9 +1,20 @@
 package com.gusdev.admin.catalogo.infrastructure.api.controllers;
 
+import com.gusdev.admin.catalogo.domain.pagination.Pagination;
 import com.gusdev.admin.catalogo.infrastructure.api.CategoryAPI;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController implements CategoryAPI {
 
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(String search, int page, int perPage, String sort, String dir) {
+        return null;
+    }
 }
