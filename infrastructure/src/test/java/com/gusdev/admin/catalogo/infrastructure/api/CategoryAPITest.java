aa0b5c4aa24785a011a9586d1ebe5a9e4e92d57c
GustavoMarques94 <marques.lameirao@gmail.com>
2024-02-06 23:44:38 -0300

    Testes integrados do endpoint de criação de categoria

--- infrastructure/src/test/java/com/gusdev/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/gusdev/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -1,9 +1,26 @@
 package com.gusdev.admin.catalogo.infrastructure.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gusdev.admin.catalogo.ControllerTest;
+import com.gusdev.admin.catalogo.application.category.create.CreateCategoryCommand;
+import com.gusdev.admin.catalogo.application.category.create.CreateCategoryOutput;
+import com.gusdev.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.gusdev.admin.catalogo.domain.category.CategoryID;
+import com.gusdev.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
+import io.vavr.API;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Objects;
 
 @ControllerTest(controllers = CategoryAPI.class)
 public class CategoryAPITest {
@@ -14,9 +31,40 @@ public class CategoryAPITest {
     @Autowired
     private MockMvc mvc;
 
-    @Test
-    public void test(){
+    @Autowired
+    private ObjectMapper mapper;
 
+    @MockBean //estou dizendo pro spring que quero que ele cria uma versão desse Bean totalmente mockavel para o spring injetar no controller
+    private CreateCategoryUseCase createCategoryUseCase;
+
+    @Test
+    public void givenAValidCommand_whenCallCreateCategory_shouldReturnCategoryId() throws Exception {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A melhor categoria de filmes";
+        final var expectedIsActive = true;
+
+        final var anInput =
+                new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        Mockito.when(createCategoryUseCase.execute(Mockito.any()))
+                .thenReturn(API.Right(CreateCategoryOutput.from(CategoryID.from("123"))));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(anInput));
+
+        this.mvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isCreated(),
+                        MockMvcResultMatchers.header().string("Location", "/categories/123")
+                );
+
+        Mockito.verify(createCategoryUseCase, Mockito.times(1)).execute(Mockito.argThat(cmd ->
+                Objects.equals(expectedName, cmd.name()) &&
+                        Objects.equals(expectedDescription, cmd.description()) &&
+                        Objects.equals(expectedIsActive, cmd.isActive())
+        ));
     }
 
 }
