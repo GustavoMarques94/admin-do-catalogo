58f5857490e5545723afc8f57a6234a3689712ed
GustavoMarques94 <marques.lameirao@gmail.com>
2024-02-13 12:50:18 -0300

    Testes integrados do endpoint de get por id de categoria

--- infrastructure/src/test/java/com/gusdev/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/gusdev/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gusdev.admin.catalogo.ControllerTest;
 import com.gusdev.admin.catalogo.application.category.create.CreateCategoryOutput;
 import com.gusdev.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.gusdev.admin.catalogo.application.category.retrieve.get.CategoryOutput;
+import com.gusdev.admin.catalogo.application.category.retrieve.get.GetCategoryByIdUseCase;
+import com.gusdev.admin.catalogo.domain.category.Category;
+import com.gusdev.admin.catalogo.domain.category.CategoryID;
 import com.gusdev.admin.catalogo.domain.exceptions.DomainException;
 import com.gusdev.admin.catalogo.domain.validation.Error;
 import com.gusdev.admin.catalogo.domain.validation.handler.Notification;
@@ -20,6 +24,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
 @ControllerTest(controllers = CategoryAPI.class)
@@ -37,9 +43,13 @@ public class CategoryAPITest {
     @MockBean //estou dizendo pro spring que quero que ele cria uma versão desse Bean totalmente mockavel para o spring injetar no controller
     private CreateCategoryUseCase createCategoryUseCase;
 
+    @MockBean
+    private GetCategoryByIdUseCase getCategoryByIdUseCase;
+
     //cenário de teste feliz, quando tudo está ok
     @Test
     public void givenAValidCommand_whenCallCreateCategory_shouldReturnCategoryId() throws Exception {
+        // given
         final var expectedName = "Filmes";
         final var expectedDescription = "A melhor categoria de filmes";
         final var expectedIsActive = true;
@@ -50,15 +60,17 @@ public class CategoryAPITest {
         Mockito.when(createCategoryUseCase.execute(Mockito.any()))
                 .thenReturn(API.Right(CreateCategoryOutput.from("123"))); //Temos que retornar um Either de API.Right, e ele espera um retorno que é o retorno do nosso caso de uso 'CategoryOutput'
 
+        // when
         final var request = MockMvcRequestBuilders.post("/categories")
                 .contentType(MediaType.APPLICATION_JSON) //Json como tipo de conteúdo
                 .content(this.mapper.writeValueAsString(anInput)); //Que Json iremos enviar? um conteúdo (CreateCategoryApiInput)
                 //O 'writeValueAsString' lança uma 'Exception' então devemos especificar no método que ele faz um throws de Exception (Como é um método de teste não tem problema)
 
-        this.mvc.perform(request) //Estou dizendo que ele irá performar uma ação, mas que ação? o 'request' instanciado a cima irá performar está requisição
-                .andDo(MockMvcResultHandlers.print())
-                //iremos passar uma lista de predicados
-                .andExpect(MockMvcResultMatchers.status().isCreated()) //Essse é o cara que nós esperamos
+        final var response = this.mvc.perform(request) //Estou dizendo que ele irá performar uma ação, mas que ação? o 'request' instanciado a cima irá performar está requisição
+                .andDo(MockMvcResultHandlers.print());
+
+        //  then
+        response.andExpect(MockMvcResultMatchers.status().isCreated()) //Essse é o cara que nós esperamos
                 .andExpect(MockMvcResultMatchers.header().string("Location", "/categories/123")) //O que espero  chegar no header
                 .andExpect(MockMvcResultMatchers.header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.equalTo("123")));
@@ -72,8 +84,10 @@ public class CategoryAPITest {
         ));
     }
 
+    //cenário de teste ruim, retorno uma notificação
     @Test
     public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnNotification() throws Exception {
+        // given
         final String expectedName = null;
         final var expectedDescription = "A melhor categoria de filmes";
         final var expectedIsActive = true;
@@ -85,15 +99,17 @@ public class CategoryAPITest {
         Mockito.when(createCategoryUseCase.execute(Mockito.any()))
                 .thenReturn(API.Left(Notification.create(new Error(expectedMessage)))); //Temos que retornar um Either de API.Left, caso de erro, como setamos o name como null, então é esse o erro que esperamos
 
+        // when
         final var request = MockMvcRequestBuilders.post("/categories")
                 .contentType(MediaType.APPLICATION_JSON) //Json como tipo de conteúdo
                 .content(this.mapper.writeValueAsString(anInput)); //Que Json iremos enviar? um conteúdo (CreateCategoryApiInput)
         //O 'writeValueAsString' lança uma 'Exception' então devemos especificar no método que ele faz um throws de Exception (Como é um método de teste não tem problema)
 
-        this.mvc.perform(request) //Estou dizendo que ele irá performar uma ação, mas que ação? o 'request' instanciado a cima irá performar está requisição
-                .andDo(MockMvcResultHandlers.print())
-                //iremos passar uma lista de predicados
-                .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity()) //Essse é o cara que nós esperamos
+        final var response = this.mvc.perform(request) //Estou dizendo que ele irá performar uma ação, mas que ação? o 'request' instanciado a cima irá performar está requisição
+                .andDo(MockMvcResultHandlers.print());
+
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity()) //Essse é o cara que nós esperamos
                 .andExpect(MockMvcResultMatchers.header().string("Location", Matchers.nullValue())) //O que espero  chegar no header
                 .andExpect(MockMvcResultMatchers.header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.errors", Matchers.hasSize(1)))
@@ -108,8 +124,10 @@ public class CategoryAPITest {
         ));
     }
 
+    //cenário de teste ruim, retorno uma Exception
     @Test
     public void givenAInvalidCommand_whenCallsCreateCategory_thenShouldReturnDomainException() throws Exception {
+        // given
         final String expectedName = null;
         final var expectedDescription = "A melhor categoria de filmes";
         final var expectedIsActive = true;
@@ -121,15 +139,17 @@ public class CategoryAPITest {
         Mockito.when(createCategoryUseCase.execute(Mockito.any()))
                 .thenThrow(DomainException.with(new Error(expectedMessage))); //Lanço uma Exception quando bater no create
 
+        // when
         final var request = MockMvcRequestBuilders.post("/categories")
                 .contentType(MediaType.APPLICATION_JSON) //Json como tipo de conteúdo
                 .content(this.mapper.writeValueAsString(anInput)); //Que Json iremos enviar? um conteúdo (CreateCategoryApiInput)
         //O 'writeValueAsString' lança uma 'Exception' então devemos especificar no método que ele faz um throws de Exception (Como é um método de teste não tem problema)
 
-        this.mvc.perform(request) //Estou dizendo que ele irá performar uma ação, mas que ação? o 'request' instanciado a cima irá performar está requisição
-                .andDo(MockMvcResultHandlers.print())
-                //iremos passar uma lista de predicados
-                .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity()) //Essse é o cara que nós esperamos
+        final var response = this.mvc.perform(request) //Estou dizendo que ele irá performar uma ação, mas que ação? o 'request' instanciado a cima irá performar está requisição
+                .andDo(MockMvcResultHandlers.print());
+
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity()) //Essse é o cara que nós esperamos
                 .andExpect(MockMvcResultMatchers.header().string("Location", Matchers.nullValue())) //O que espero  chegar no header
                 .andExpect(MockMvcResultMatchers.header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.errors", Matchers.hasSize(1)))
@@ -143,6 +163,58 @@ public class CategoryAPITest {
                         Objects.equals(expectedDescription, cmd.description()) &&
                         Objects.equals(expectedIsActive, cmd.isActive())
         ));
+    }
+
+    @Test
+    public void givenAValidId_whenCallsGetCategory_shouldReturnCategory() throws Exception{
+        // given
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A melhor categoria de filmes";
+        final var expectedIsActive = true;
+
+        final var aCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var expectedId = aCategory.getId().getValue();
+
+        Mockito.when(getCategoryByIdUseCase.execute(Mockito.any()))
+                .thenReturn(CategoryOutput.from(aCategory));
+
+        // when
+        final var request = MockMvcRequestBuilders.get("/categories/{id}", expectedId);
+
+        final var response = this.mvc.perform(request)
+                .andDo(MockMvcResultHandlers.print());
+
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.equalTo(expectedId)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.equalTo(expectedName)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.description", Matchers.equalTo(expectedDescription)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.is_active", Matchers.equalTo(expectedIsActive)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.created_at", Matchers.equalTo(aCategory.getCreatedAt().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.updated_at", Matchers.equalTo(aCategory.getUpdatedAt().toString())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.deleted_at", Matchers.equalTo(aCategory.getDeletedAt().toString())));
+
+        Mockito.verify(getCategoryByIdUseCase, Mockito.times(1)).execute(Mockito.eq(expectedId));
+    }
+
+    @Test
+    public void givenAInvalidId_whenCallsGetCategory_shouldReturnNotFound() throws Exception{
+        // given
+        final var expetedErrorMessage = "Category with ID 123 was not-found";
+        final var expectedId = CategoryID.from("123").getValue();
+
+        // when
+        final var request = MockMvcRequestBuilders.get("/categories/{id}", expectedId);
+
+        final var response = this.mvc.perform(request)
+                .andDo(MockMvcResultHandlers.print());
+
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.equalTo(expetedErrorMessage)));
+
     }
 
 }
